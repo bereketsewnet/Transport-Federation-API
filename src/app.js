86995c62b8732d0e984505ec3887724ca5cd3e35
bereketsewnet <bereketsewnet306@gmail.com>
2025-10-16 23:09:53 +0300

    i add seeding

--- src/app.js
+++ src/app.js
@@ -2,8 +2,15 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const cors = require('cors');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 // auth
 app.use('/api/auth', require('./routes/auth.routes'));
